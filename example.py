bffd54264a3e81d917728ac4596fc9aa8cdb002c
Ivan de Paz Centeno <ipazc@unileon.es>
2018-01-05 05:09:47 +0100

    Initial commit of the full 0.0.1 version of mtcnn for PIP.

---
+++ example.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cv2
+from mtcnn.mtcnn import MTCNN
+
+detector = MTCNN()
+
+image = cv2.imread("ivan.jpg")
+result = detector.detect_faces(image)
+
+# Result is an array with all the bounding boxes detected. We know that for 'ivan.jpg' there is only one.
+bounding_box = result[0]['box']
+keypoints = result[0]['keypoints']
+
+cv2.rectangle(image,
+              (bounding_box[0], bounding_box[1]),
+              (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
+              (0,155,255),
+              2)
+
+cv2.circle(image,(keypoints['left_eye']), 2, (0,155,255), 2)
+cv2.circle(image,(keypoints['right_eye']), 2, (0,155,255), 2)
+cv2.circle(image,(keypoints['nose']), 2, (0,155,255), 2)
+cv2.circle(image,(keypoints['mouth_left']), 2, (0,155,255), 2)
+cv2.circle(image,(keypoints['mouth_right']), 2, (0,155,255), 2)
+
+cv2.imwrite("ivan_drawn.jpg", image)
+
+print(result)
---
+++ setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#MIT License
+#
+#Copyright (c) 2018 Iván de Paz Centeno
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+import sys
+from setuptools import setup, setuptools
+from mtcnn import __version__
+
+__author__ = 'Iván de Paz Centeno'
+
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+if sys.version_info < (3, 4, 1):
+    sys.exit('Python < 3.4.1 is not supported!')
+
+
+setup(name='mtcnn',
+      version=__version__,
+      description='Multi-task Cascaded Convolutional Neural Networks for Face Detection, based on TensorFlow',
+      long_description=readme(),
+      url='http://github.com/ipazc/mtcnn',
+      author='Iván de Paz Centeno',
+      author_email='ipazc@unileon.es',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=[
+          "opencv-python",
+          "tensorflow"
+      ],
+      classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      keywords="mtcnn face detection tensorflow pip package",
+      zip_safe=False)
