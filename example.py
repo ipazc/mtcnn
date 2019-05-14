6fca43259bbc6d7acca350a62f40756d8c9af2f0
Marco Mercado <mmerc007@ucr.edu>
2019-05-14 09:42:32 -0700

    update

--- example.py
+++ example.py
@@ -14,20 +14,20 @@ end = time.time()
 print (end - start)
 print("test")
 # Result is an array with all the bounding boxes detected. We know that for 'ivan.jpg' there is only one.
-bounding_box = result[0]['box']
-keypoints = result[0]['keypoints']
 
-cv2.rectangle(image,
-              (bounding_box[0], bounding_box[1]),
-              (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
-              (0,155,255),
-              2)
-
-cv2.circle(image,(keypoints['left_eye']), 2, (0,155,255), 2)
-cv2.circle(image,(keypoints['right_eye']), 2, (0,155,255), 2)
-cv2.circle(image,(keypoints['nose']), 2, (0,155,255), 2)
-cv2.circle(image,(keypoints['mouth_left']), 2, (0,155,255), 2)
-cv2.circle(image,(keypoints['mouth_right']), 2, (0,155,255), 2)
+for i in len(result):
+    bounding_box = result[0]['box']
+    keypoints = result[0]['keypoints']
+    cv2.rectangle(image,
+                  (bounding_box[0], bounding_box[1]),
+                  (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
+                  (0,155,255),
+                  2)
+    cv2.circle(image,(keypoints['left_eye']), 2, (0,155,255), 2)
+    cv2.circle(image,(keypoints['right_eye']), 2, (0,155,255), 2)
+    cv2.circle(image,(keypoints['nose']), 2, (0,155,255), 2)
+    cv2.circle(image,(keypoints['mouth_left']), 2, (0,155,255), 2)
+    cv2.circle(image,(keypoints['mouth_right']), 2, (0,155,255), 2)
 
 cv2.imwrite("ivan_drawn.jpg", image)
 
