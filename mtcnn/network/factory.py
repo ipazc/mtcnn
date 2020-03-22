f1a7ec96f3897d090ef2900e380d6018f04f03ec
Kash Nouroozi <knouroozi@gmail.com>
2020-03-22 19:26:21 -0400

    So you can use this as a library.

--- mtcnn/network/factory.py
+++ mtcnn/network/factory.py
@@ -23,8 +23,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from tf.keras.layers import Input, Dense, Conv2D, MaxPooling2D, PReLU, Flatten, Softmax
-from tf.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, PReLU, Flatten, Softmax
+from tensorflow.keras.models import Model
 
 import numpy as np
 
